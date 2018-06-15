22c5b0f7480a3f5a53b469f840d7814013f9dab7
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2018-06-15 14:42:42 +0200

    playback now with my own fm2 parser

--- playback.py
+++ playback.py
@@ -1,6 +1,7 @@
 import retro
 import matplotlib
 
+from forked_lib.parse_fm2 import parse_fm2
 from small_evo.wrappers import AutoRenderer
 
 matplotlib.use('TkAgg')
@@ -8,33 +9,35 @@ import matplotlib.pyplot as plt
 
 from reward_plot import RewardPlotter, make_axes
 
-movie_name = 'movies/1-1_basic_5.fm2'
-movie = retro.Movie(movie_name)
+movie_name = '/home/peer/mario/1-1_shortcut_2.fm2'
+playback_offset = 0
+retro_state = retro.State.NONE
+with open(movie_name) as movie_file:
+    inputs = list(parse_fm2(movie_file))
 
-env = retro.make("SuperMarioBros-Nes", retro.State.NONE, use_restricted_actions=retro.Actions.ALL)
-env.reset()
-env.reset()
+env = retro.make("SuperMarioBros-Nes", retro_state, use_restricted_actions=retro.Actions.ALL)
 env = AutoRenderer(env, 1)
+env.reset()
+env.reset()
 
 axes = make_axes()
-plotter = RewardPlotter(axes, save_period=4, render_period=20, max_entries=3000, sum_reward_up=True)
+plotter = RewardPlotter(axes, save_period=6, render_period=20, max_entries=3000, sum_reward_up=True)
 
-frame_counter = 0
 rewards = []
-while movie.step():
-    keys = []
-    for i in range(env.unwrapped.num_buttons):
-        keys.append(movie.get_key(i, 0))
+buttons = [name.upper() if name is not None else None for name in env.unwrapped.buttons]
+for frame_counter, input_dict in enumerate(inputs[playback_offset:]):
+    keys = [input_dict[name] if name is not None else False for name in buttons]
+    # for i in range(env.unwrapped.num_buttons):
+    #     keys.append(movie.get_key(i, 0))
     _obs, rew, _done, _info = env.step(keys)
     rewards.append(rew)
     plotter.update(rew, frame_counter)
-    # if frame_counter % 2 == 0:
-    # env.render()
-    frame_counter += 1
 print("total reward: {}".format(sum(rewards)))
 # pandas.Series(rewards).plot()
 
 env.render("close")
 env.close()
 plt.savefig(movie_name[:-3] + "pdf")
+with open(movie_name[:-3] + "info", "a+") as info_file:
+    info_file.write("total reward: {}\n".format(sum(rewards)))
 plt.close()
--- utils/sync.py
+++ utils/sync.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 from reward_plot import RewardPlotter, make_axes
 
-movie_name = 'movies/5_1-1_without-shortcut.fm2'
+movie_name = 'movies/6_1-1_with_shortcut.fm2'
 with open(movie_name) as movie_file:
     inputs = list(parse_fm2(movie_file))
 
@@ -19,7 +19,9 @@ env.reset()
 
 rewards = []
 buttons = [name.upper() if name is not None else None for name in env.unwrapped.buttons]
-for offset in range(200, 250, 1):
+search_start = 280
+search_end = 350
+for offset in range(search_start, search_end, 1):
     env.reset()
     episode_reward = 0
     for frame_counter, input_dict in enumerate(inputs[offset:]):
@@ -35,6 +37,6 @@ for offset in range(200, 250, 1):
 print(rewards)
 env.close()
 # plt.savefig(movie_name[:-3] + "pdf")
-# with open(movie_name[:-3] + "info") as info_file:
-#     info_file.write("total reward: {}".format(sum(rewards)))
+with open(movie_name[:-3] + "info", "a+") as info_file:
+    info_file.write("default state offset: {}; at: {}\n".format(rewards.index(max(rewards)) + search_start, max(rewards)))
 plt.close()
