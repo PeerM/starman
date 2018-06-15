a47cb24df166930ae632387309418f24c063d6d0
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2018-06-15 13:59:10 +0200

    automatic syncing of movies

---
+++ utils/sync.py
@@ -0,0 +1,40 @@
+import retro
+import matplotlib
+
+from forked_lib.parse_fm2 import parse_fm2
+from small_evo.wrappers import AutoRenderer
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+from reward_plot import RewardPlotter, make_axes
+
+movie_name = 'movies/5_1-1_without-shortcut.fm2'
+with open(movie_name) as movie_file:
+    inputs = list(parse_fm2(movie_file))
+
+env = retro.make("SuperMarioBros-Nes", retro.State.DEFAULT, use_restricted_actions=retro.Actions.ALL)
+env.reset()
+env.reset()
+
+rewards = []
+buttons = [name.upper() if name is not None else None for name in env.unwrapped.buttons]
+for offset in range(200, 250, 1):
+    env.reset()
+    episode_reward = 0
+    for frame_counter, input_dict in enumerate(inputs[offset:]):
+        keys = [input_dict[name] if name is not None else False for name in buttons]
+        # for i in range(env.unwrapped.num_buttons):
+        #     keys.append(movie.get_key(i, 0))
+        _obs, rew, _done, _info = env.step(keys)
+        episode_reward += rew
+    rewards.append(episode_reward)
+    print("episode offset: {}; reward: {}".format(offset, episode_reward))
+    # pandas.Series(rewards).plot()
+
+print(rewards)
+env.close()
+# plt.savefig(movie_name[:-3] + "pdf")
+# with open(movie_name[:-3] + "info") as info_file:
+#     info_file.write("total reward: {}".format(sum(rewards)))
+plt.close()
