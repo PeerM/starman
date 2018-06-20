9ca7fded8704627aea50e546ff8b38176c746a4a
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2018-06-20 12:19:21 +0200

    new run

--- rl_baseline/rainbow_agent.py
+++ rl_baseline/rainbow_agent.py
@@ -24,14 +24,14 @@ def handle_ep(steps, reward):
 
 def main():
     """Run DQN until the environment throws an exception."""
-    base_path = "results/rainbow/4/"
+    base_path = "results/rainbow/6/"
     env = make_env(stack=False, scale_rew=False, render=None, monitor=base_path + "train_monitor",
                    episodic_life=True)
     # I think the env itself allows Backtracking
     env = BatchedFrameStack(BatchedGymEnv([[env]]), num_images=4, concat=False)
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    config.gpu_options.per_process_gpu_memory_fraction = 0.6
+    config.gpu_options.per_process_gpu_memory_fraction = 0.8
 
     with tf.Session(config=config) as sess:
         dqn = DQN(*rainbow_models(sess,
@@ -39,22 +39,23 @@ def main():
                                   min_val=-200, max_val=200))
         player = NStepPlayer(BatchedPlayer(env, dqn.online_net), 3)
         optimize = dqn.optimize(learning_rate=1e-4)
-        saver = tf.train.Saver(name="rainbow", keep_checkpoint_every_n_hours=1)
+        saver = tf.train.Saver(name="rainbow")
         sess.run(tf.global_variables_initializer())
         saver.save(sess, base_path + "training", global_step=0)
         try:
-            dqn.train(num_steps=1_000_000,  # Make sure an exception arrives before we stop.
+            dqn.train(num_steps=2_000_000,  # Make sure an exception arrives before we stop.
                       player=player,
                       replay_buffer=PrioritizedReplayBuffer(500000, 0.5, 0.4, epsilon=0.1),
                       optimize_op=optimize,
                       train_interval=1,
                       target_interval=8192,
-                      batch_size=32,
+                      batch_size=64,
                       min_buffer_size=20000,
                       handle_ep=handle_ep)  # in seconds
         except KeyboardInterrupt:
-            print("finishing")
-            saver.save(sess, base_path + "final", global_step=1_000_000)
+            print("keyboard interrupt")
+        print("finishing")
+        saver.save(sess, base_path + "final", global_step=2_000_000)
 
 
 if __name__ == '__main__':
