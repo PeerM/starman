f6224aa8cca114578efefcc8fc5cdf7c97fdf881
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2018-06-22 17:34:45 +0200

    some stats

--- rl_baseline/rainbow_run.py
+++ rl_baseline/rainbow_run.py
@@ -27,17 +27,17 @@ from rl_baseline.mario_util import AllowBacktracking, make_env
 def main():
     """Run DQN until the environment throws an exception."""
     # "results/rainbow/2/videos/6"
-    save_dir = "results/rainbow/6/val_monitor/2"
-    env = make_env(stack=False, scale_rew=False, render=32, monitor=save_dir,
+    save_dir = "results/rainbow/7/val_monitor/1"
+    env = make_env(stack=False, scale_rew=False, render=3, monitor=None,
                    timelimit=False, episodic_life=True, video=lambda id: True)
     # env = AllowBacktracking(make_env(stack=False, scale_rew=False))
     env = BatchedFrameStack(BatchedGymEnv([[env]]), num_images=4, concat=False)
     config = tf.ConfigProto()
 
     with tf.Session(config=config) as sess:
-        saver = tf.train.import_meta_graph("results/rainbow/6/final-2000000.meta", clear_devices=True)
+        saver = tf.train.import_meta_graph("results/rainbow/7/final-4000000.meta", clear_devices=True)
         # saver.restore(sess, tf.train.latest_checkpoint('results/rainbow/2'))
-        saver.restore(sess, 'results/rainbow/6/final-2000000')
+        saver.restore(sess, 'results/rainbow/7/final-4000000')
         model = LoadedNetwork(sess, gym_space_vectorizer(env.observation_space))
         # rebuild the online_net form the saved model
         # type <anyrl.models.dqn_dist.NatureDistQNetwork object at ???>
@@ -46,7 +46,7 @@ def main():
         with tf.device("/cpu"):
             # sess.run(tf.global_variables_initializer())
             try:
-                for episode_index in tqdm(range(20), unit="episode"):
+                for episode_index in tqdm(range(6), unit="episode"):
                     axes = make_axes()
                     plotter = RewardPlotter(axes, save_period=20, render_period=300, max_entries=600)
                     for i in count():
@@ -61,7 +61,7 @@ def main():
                         if end_of_episode:
                             # plt.show()
                             plotter.render()
-                            plotter.save_file("{}/e{}.pdf".format(save_dir, episode_index))
+                            # plotter.save_file("{}/e{}.pdf".format(save_dir, episode_index))
                             plotter.close()
                             break
             except KeyboardInterrupt:
